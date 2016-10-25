83d2b94de5410aab46f79750b643e5fb6906de27
Nickolas Howell <cade177@gmail.com>
2016-10-25 12:10:14 -0600

    Imported version 1.0
    Now I need to rework the files so they include an easy to use method.

---
+++ EasyAsciiEncrypt.py
@@ -0,0 +1,8 @@
+__author__ = 'Nickolas Howell'
+from lib import * #Imports important data, along with all the functions needed to operate encryption application.
+
+for value, key in keyLibrary.iteritems():
+        wordsUsed[key] = 0
+clearWords()
+errorReport()
+main()
