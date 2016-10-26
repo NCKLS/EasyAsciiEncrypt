225f1e3f308cb28a8764ebc6619e2506ed209e0e
Nickolas Howell <cade177@gmail.com>
2016-10-26 10:14:16 -0600

    Working on github setup
    Almost complete

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+      name='EasyAsciiEncrypt',
+      version='1.1',
+      description='Encryption module created by Nickolas',
+      author='Nickolas Howell',
+      author_email='nickolas.howell@icloud.com',
+      url='https://www.python.org/',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False
+)
